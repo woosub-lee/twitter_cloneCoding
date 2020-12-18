d39e6da4f2583d8630e3c7411d83171df52a86a9
woosub-lee <woosub.project@gmail.com>
2020-12-18 09:51:40 +0900

    add link button

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -14,10 +14,33 @@ const Home = ({userObj}) => {
             setCtweets(CtweetArray);
         })
     },[]);
-    
+    const onYoutubeClick = () => {
+        window.open(`https://youtube.com`);
+    }
+    const onGoogleClick = () => {
+        window.open(`https://google.com/`);
+    }
+    const onClassroomClick = () => {
+        window.open(`https://classroom.google.com/`);
+    }
+    const onGithubClick = () => {
+        window.open(`https://github.com/`);
+    }
     return (
         <div className="container">
             <CtweetWriter userObj={userObj}/>
+            <span onClick={onYoutubeClick} className="ClickBtn RedBtn">
+                Go To Youtube
+            </span>
+            <span onClick={onGoogleClick} className="ClickBtn GreenBtn">
+                Go To Google
+            </span>
+            <span onClick={onClassroomClick} className="ClickBtn">
+                Go To ClassRoom
+            </span>
+            <span onClick={onGithubClick} className="ClickBtn WhiteBtn">
+                Go To Github
+            </span>
             <div style={{ marginTop: 30 }}>
                 {ctweets.map((ctweet) => (
                     <Ctweet key={ctweet.id} CtweetObj={ctweet} isOwner={ctweet.creatorId === userObj.uid}/>
